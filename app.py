9b19d63dd5c53aedd5ba1bc7f994b4fdd71a27f7
montatip07 <51740573+montatip07@users.noreply.github.com>
2019-10-02 12:27:03 +0700

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def callback():
     #sendText(user,userText)
     if (usertext)== 'สวัสดี') :
         sendtext(user,'ไอ้บ้า')
-    elif (usertext == 'หิวไหม'):
+    elif (usertext == 'หิวไหม') :
         sendtext(user,'สวยนะ')
     else :
         sendtext(user,'ลาวอีก')
