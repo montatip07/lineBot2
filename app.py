b3ad4ea92830f4eecb5aac210ac7455e70486063
montatip07 <51740573+montatip07@users.noreply.github.com>
2019-10-02 11:07:24 +0700

    Update app.py

--- app.py
+++ app.py
@@ -22,14 +22,14 @@ def callback():
     decoded = json.loads(json_line)
     user = decoded['originalDetectIntentRequest']['payload']['data']['replyToken']
     userText = decoded['queryResult']['intent']['displayName']
-   
-   if (usertext)== 'สวัสดี') :
+    if (usertext)== 'สวัสดี') :
        sendtext(user,'ไอ้บ้า')
    elif (usertext == 'หิวไหม') :
        sendtext(user,'สวยนะ')
    else :
        sendtext(user,'ลาวอีก')
    return '',200
+  
 
 def sendText(user, text):
   LINE_API = 'https://api.line.me/v2/bot/message/reply'
