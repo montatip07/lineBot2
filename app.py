35d83915b2090c9b136f2b8cceaba2ef937a70f6
montatip07 <51740573+montatip07@users.noreply.github.com>
2019-10-02 10:51:18 +0700

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def callback():
        sendtext(user,'สวยนะ')
    else :
        sendtext(user,'ลาวอีก')
-    return '',200
+   return '',200
 
 def sendText(user, text):
   LINE_API = 'https://api.line.me/v2/bot/message/reply'
