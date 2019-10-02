39f2dc48bd6ac634fa345d9d69045805ca8813b5
montatip07 <51740573+montatip07@users.noreply.github.com>
2019-10-02 13:07:12 +0700

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def callback():
         sendtext(user,'ลาวอีก')
     return '',200
    
-  def sendText(user, text):
+def sendText(user, text):
   LINE_API = 'https://api.line.me/v2/bot/message/reply'
   headers = {
     'Content-Type': 'application/json; charset=UTF-8',
